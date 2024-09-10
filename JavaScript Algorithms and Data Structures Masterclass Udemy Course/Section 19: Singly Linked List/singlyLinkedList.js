50e239587f7e6ca0111f92f50270082e96f71632
Andrew Tran <andrewdaotran@gmail.com>
2024-09-09 22:59:06 -0700

    Add shift, unshift, and get to singlyLinkedList.js

--- JavaScript Algorithms and Data Structures Masterclass Udemy Course/Section 19: Singly Linked List/singlyLinkedList.js
+++ JavaScript Algorithms and Data Structures Masterclass Udemy Course/Section 19: Singly Linked List/singlyLinkedList.js
@@ -50,6 +50,48 @@ class SinglyLinkedList {
 		}
 		return tail
 	}
+
+	shift() {
+		// get the value of the head
+		// make the this.head.next the new head
+		// return the old head
+		if (!this.head) return undefined
+		const head = this.head
+		this.head = head.next
+		head.next = null
+		this.length--
+		if (this.length === 1) this.tail = null
+		return head
+	}
+	unshift(val) {
+		// create a new node
+		// make the new node.next the old head
+		// make the new head the new node
+		const newHead = new Node(val)
+		if (!this.head) {
+			this.head = newHead
+			this.tail = newHead
+		} else {
+			newHead.next = this.head
+			this.head = newHead
+		}
+		this.length++
+		return this
+	}
+
+	get(pos) {
+		// loop through the linked list until we reach the position
+		// return the node
+		if (pos < 0 || pos >= this.length) return null
+		if (pos === 0) return this.head
+		let counter = 1
+		let currentNode = this.head.next
+		while (counter < pos) {
+			currentNode = currentNode.next
+			counter++
+		}
+		return currentNode
+	}
 }
 
 // const linkedList = new SinglyLinkedList()
@@ -62,3 +104,4 @@ class SinglyLinkedList {
 
 // console.log(linkedList.pop())
 // console.log(linkedList)
+// console.log(linkedList.get(5))
